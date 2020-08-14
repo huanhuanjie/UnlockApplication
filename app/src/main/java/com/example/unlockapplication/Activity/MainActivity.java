4c0f910d3bb2f981a7f8baf9a00b82f01ad50f5e
huanhuanjie <you@example.com>
2020-08-14 17:24:53 +0800

    get date and time

--- app/src/main/java/com/example/unlockapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/unlockapplication/Activity/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.unlockapplication.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.example.unlockapplication.R;
 import com.example.unlockapplication.Util.MyGridViewAdapter;
+import com.example.unlockapplication.Util.ObtainTime;
 import com.example.unlockapplication.entity.GridViewItem;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ObtainTime obtainTime;
     ArrayList<GridViewItem> datas = new ArrayList<>();
     int[] images;
 
@@ -21,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GridView gridView = findViewById(R.id.gridview_numbers);
+        TextView time = findViewById(R.id.time);
+        TextView date = findViewById(R.id.date);
+        TextView week = findViewById(R.id.week);
+        TextView lunar_date = findViewById(R.id.lunar_date);
+
+        obtainTime = new ObtainTime();
+        time.setText(obtainTime.getTime());
+        date.setText(obtainTime.getDate());
+        week.setText(obtainTime.getWeek());
+        //lunar_date.setText(obtainTime.getWeek());
+
         images = new int[]{R.drawable.num1,R.drawable.num2,R.drawable.num3,
                 R.drawable.num4,R.drawable.num5,R.drawable.num6,
                 R.drawable.num7,R.drawable.num8,R.drawable.num9,
--- app/src/main/java/com/example/unlockapplication/Util/ObtainTime.java
+++ app/src/main/java/com/example/unlockapplication/Util/ObtainTime.java
@@ -1,5 +1,56 @@
 package com.example.unlockapplication.Util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class ObtainTime {
+
+    long currentDate;
+    SimpleDateFormat simpleDateFormat;
+
+    public ObtainTime() {
+        this.currentDate = System.currentTimeMillis();
+    }
+
+    public String getTime(){
+        simpleDateFormat = new SimpleDateFormat("HH:mm");
+        String time = simpleDateFormat.format(currentDate);
+        return time;
+    }
+
+    public String getDate(){
+        simpleDateFormat = new SimpleDateFormat("MM月dd日");
+        String date = simpleDateFormat.format(currentDate);
+        return date;
+    }
+
+    public String getWeek(){
+        Calendar cal = Calendar.getInstance();
+        int i = cal.get(Calendar.DAY_OF_WEEK);
+        switch (i) {
+            case 1:
+                return "星期日";
+            case 2:
+                return "星期一";
+            case 3:
+                return "星期二";
+            case 4:
+                return "星期三";
+            case 5:
+                return "星期四";
+            case 6:
+                return "星期五";
+            case 7:
+                return "星期六";
+            default:
+                return "";
+        }
+    }
+
+    /*public String getLunarDate(){
+        simpleDateFormat = new SimpleDateFormat("MM月dd日");
+        String date = simpleDateFormat.format(currentDate);
+        return date;
+    }*/
 
 }
